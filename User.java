e1680239478b096b3448856452bb7a78411ca5a8
Abigail-Johanna <rualesabigail09@gmail.com>
2025-03-06 00:19:22 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,22 +1,46 @@
 
 package medicationtracker;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class User {
     private String username;
-    private String password;
+    private String passwordHash; 
     private String role;
 
     public User(String username, String password, String role) {
         this.username = username;
-        this.password = password;
+        this.passwordHash = hashPassword(password); 
         this.role = role;
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public String getRole() { return role; }
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
 
     public boolean validatePassword(String inputPassword) {
-        return this.password.equals(inputPassword);
+        return passwordHash.equals(hashPassword(inputPassword)); 
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                hexString.append(String.format("%02x", b));
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
     }
 }
+
